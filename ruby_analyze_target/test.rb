27418bd291f81cc688de2dc492a77c5cb644284f
Zhu <lszhu@suse.com>
2017-01-04 23:59:06 +0800

    can find eui and iqn targets, need to combine them together

--- ruby_analyze_target/test.rb
+++ ruby_analyze_target/test.rb
@@ -24,12 +24,14 @@ end
 file = File.open("target.txt","r")
 str = File.readlines(file)    #This is an array
 #puts str
-re = Regexp.new("iqn\.\d{4}-\d{2}\.[\w.]+\s\.*\s\[TPGs:\s\d+\]'\|'eui\.\w+\s\.*\s\[TPGs:\s\d+\]")
-
-#test = str.split(/'iqn'/)
-if re =~"iqn.2016-12.suse.com.lszhu"
-   puts "matched"
-end
+re_iqn_target = Regexp.new(/iqn\.\d{4}\-\d{2}\.[\w\.]+\s\.+\s\[TPGs:\s\d+\]/)
+#re = Regexp.new(/iqn\.\d{4}-\d{2}\.[\w.]+\s\.*\s\[TPGs:\s\d+\]'\|'eui\.\w+\s\.*\s\[TPGs:\s\d+\]/)
+#a = /iqn\.\d{4}-\d{2}\.[\w\.]+\s\.+\s\[TPGs:\s\d+\]/.match("iqn.2016-12.tst.com ..... [TPGs: 1]")
+iqn_results = re_iqn_target.match("iqn.2017-01.suse.com ........... [TPGs: 12]")
+puts iqn_results
+re_eui_target = Regexp.new(/eui\.\w+\s\.+\s\[TPGs:\s\d+\]/)
+eui_results = re_eui_target.match("eui.abcdef00123 ............... [TPGs: 22]")
+puts eui_results
 TargetList = []
 
 test = Target.new
