1c8857de0fe690cda5b5662dcce22e8e6e0b7431
Zhu Lingshan <lszhu@suse.com>
2017-01-05 01:33:55 +0800

    update target.txt, add eui targets, need to make regexp work in test.rb

--- ruby_analyze_target/test.rb
+++ ruby_analyze_target/test.rb
@@ -22,9 +22,14 @@ class Target
 end
 
 file = File.open("target.txt","r")
-str = File.readlines(file)
+str = File.readlines(file)    #This is an array
 #puts str
+re = Regexp.new("iqn\.\d{4}-\d{2}\.[\w.]+\s\.*\s\[TPGs:\s\d+\]'\|'eui\.\w+\s\.*\s\[TPGs:\s\d+\]")
 
+#test = str.split(/'iqn'/)
+if re =~"iqn.2016-12.suse.com.lszhu"
+   puts "matched"
+end
 TargetList = []
 
 test = Target.new
